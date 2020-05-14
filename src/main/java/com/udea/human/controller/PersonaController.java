bddebe2613e50e2af42e0127a3d9eceddea0dc69
Diego Botia <diego.botia@gmail.com>
2020-05-14 17:29:29 -0500

    Ejemplo Spring-Boot Docker

--- src/main/java/com/udea/human/controller/PersonaController.java
+++ src/main/java/com/udea/human/controller/PersonaController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.udea.human.controller;
 
 import com.udea.human.exception.ModelNotFoundException;
